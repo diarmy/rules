1ee0ade89e84963d1856943d963aa25edcd8f5ab
Diarmuid Simpson <diarmuid@clickclickclick.co.uk>
2014-06-08 19:55:39 +0100

    Initial commit for data comparison condition and associated test

---
+++ src/Plugin/Condition/DataComparison.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\rules\Plugin\Condition\NodeIsPromoted.
+ */
+
+namespace Drupal\rules\Plugin\Condition;
+
+use Drupal\Core\TypedData\TypedDataManager;
+use Drupal\rules\Context\ContextDefinition;
+use Drupal\rules\Engine\RulesConditionBase;
+
+/**
+ * Provides a 'Data comparison' condition.
+ *
+ * @Condition(
+ *   id = "rules_data_comparison",
+ *   label = @Translation("Compare data")
+ * )
+ *
+ * @todo: Add access callback information from Drupal 7.
+ * @todo: Add group information from Drupal 7.
+ * @todo: Add public function getContextDefinitions().
+ */
+class DataComparison extends RulesConditionBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function contextDefinitions(TypedDataManager $typed_data_manager) {
+    $contexts['data'] = ContextDefinition::create($typed_data_manager, 'any')
+      ->setLabel(t('Data'));
+
+    $contexts['op'] = ContextDefinition::create($typed_data_manager, 'string')
+      ->setLabel(t('Operator'));
+
+    $contexts['value'] = ContextDefinition::create($typed_data_manager, 'any')
+      ->setLabel(t('Value'));
+
+    return $contexts;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function summary() {
+    return $this->t('Compare data');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function evaluate() {
+    $data = $this->getContextValue('data');
+    $op = $this->getContextValue('op');
+    $value = $this->getContextValue('value');
+    
+    switch ($op) {
+      default:
+      case '==':
+        // In case both values evaluate to FALSE, further differentiate between
+        // NULL values and values evaluating to FALSE.
+        if (!$data && !$value) {
+          return (isset($data) && isset($value)) || (!isset($data) && !isset($value));
+        }
+        return $data == $value;
+      case '<':
+        return $data < $value;
+      case '>':
+        return $data > $value;
+        // Note: This is deprecated by the text comparison condition and IN below.
+      case 'contains':
+        return is_string($data) && strpos($data, $value) !== FALSE || is_array($data) && in_array($value, $data);
+      case 'IN':
+        return is_array($value) && in_array($data, $value);
+    }
+  }
+
+}
---
+++ src/Tests/Condition/DataComparisonTest.php
@@ -0,0 +1,200 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\rules\Tests\Plugin\Condition.
+ */
+
+namespace Drupal\rules\Tests\Condition;
+
+use Drupal\simpletest\KernelTestBase;
+
+/**
+ * Tests the 'Data is' condition.
+ */
+class DataComparisonTest extends KernelTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = ['rules'];
+
+  /**
+   * The condition manager.
+   *
+   * @var \Drupal\Core\Condition\ConditionManager
+   */
+  protected $conditionManager;
+
+  /**
+   * The node storage.
+   *
+   * @var \Drupal\node\NodeStorage
+   */
+  protected $nodeStorage;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getInfo() {
+    return [
+      'name' => 'Data comparison condition tests',
+      'description' => 'Tests that data comparisons are true.',
+      'group' => 'Rules conditions',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp() {
+    parent::setUp();
+    $this->conditionManager = $this->container->get('plugin.manager.condition', $this->container->get('container.namespaces'));
+  }
+
+  /**
+   * Tests evaluating the condition.
+   */
+  public function testConditionEvaluation() {
+
+    // Test that when the data string equals the value string and the operator
+    // is '==', TRUE is returned
+    $test_string_data = 'Llama';
+    $test_string_value = 'Llama';
+    $condition = $this->conditionManager->createInstance('rules_data_comparison')
+      ->setContextValue('data', $test_string_data)
+      ->setContextValue('op', '==')
+      ->setContextValue('value', $test_string_value);
+    $this->assertTrue($condition->execute());
+
+    // Test that when the data string does not equal the value string and the
+    // operator is '==', FALSE is returned
+    $test_string_data = 'Kitten';
+    $condition = $this->conditionManager->createInstance('rules_data_comparison')
+      ->setContextValue('data', $test_string_data)
+      ->setContextValue('op', '==')
+      ->setContextValue('value', $test_string_value);
+    $this->assertFalse($condition->execute());
+
+    // Test that when the data string contains the value string, and the operator
+    //is 'CONTAINS', TRUE is returned
+    $test_string_data = 'Big Llama';
+    $test_string_value = 'Llama';
+    $condition = $this->conditionManager->createInstance('rules_data_comparison')
+      ->setContextValue('data', $test_string_data)
+      ->setContextValue('op', 'contains')
+      ->setContextValue('value', $test_string_value);
+    $this->assertTrue($condition->execute());
+
+    // Test that when the data string does not contain the value string, and
+    // the operator is 'contains', TRUE is returned
+    $test_string_value = 'Big Kitten';
+    $condition = $this->conditionManager->createInstance('rules_data_comparison')
+      ->setContextValue('data', $test_string_data)
+      ->setContextValue('op', 'contains')
+      ->setContextValue('value', $test_string_value);
+    $this->assertFalse($condition->execute());
+
+
+    // Test that when both data and value are false booleans and the operator
+    // is '==', TRUE is returned
+    $test_boolean_data = FALSE;
+    $test_boolean_value = FALSE;
+    $condition = $this->conditionManager->createInstance('rules_data_comparison')
+      ->setContextValue('data', $test_boolean_data)
+      ->setContextValue('op', '==')
+      ->setContextValue('value', $test_boolean_value);
+    $this->assertTRUE($condition->execute());
+
+    // Test that when a boolean data does not equal a boolean value
+    // and the operator is '==', FALSE is returned
+    $test_boolean_value = TRUE;
+    $condition = $this->conditionManager->createInstance('rules_data_comparison')
+      ->setContextValue('data', $test_boolean_data)
+      ->setContextValue('op', '==')
+      ->setContextValue('value', $test_boolean_value);
+    $this->assertFALSE($condition->execute());
+
+
+    // Test that when a data array contains the value string, and the operator 
+    // is 'CONTAINS', TRUE is returned
+    $test_array_data = array('Llama', 'Kitten');
+    $test_string_value = 'Llama';
+    $condition = $this->conditionManager->createInstance('rules_data_comparison')
+      ->setContextValue('data', $test_array_data)
+      ->setContextValue('op', 'contains')
+      ->setContextValue('value', $test_string_value);
+    $this->assertTRUE($condition->execute());
+
+    // Test that when a data array does not contain the value array, and the
+    // operator is 'CONTAINS', TRUE is returned
+    $test_array_data = array('Kitten');
+    $test_array_value = array('Llama');
+    $condition = $this->conditionManager->createInstance('rules_data_comparison')
+      ->setContextValue('data', $test_array_data)
+      ->setContextValue('op', 'contains')
+      ->setContextValue('value', $test_array_value);
+    $this->assertFALSE($condition->execute());
+
+    // Test that when the data string is 'IN' the value array, TRUE is returned
+    $test_string_data = 'Llama';
+    $test_array_value = array('Llama', 'Kitten');
+    $condition = $this->conditionManager->createInstance('rules_data_comparison')
+      ->setContextValue('data', $test_string_data)
+      ->setContextValue('op', 'IN')
+      ->setContextValue('value', $test_array_value);
+    $this->assertTRUE($condition->execute());
+
+    // Test that when the data array is not in the value array, and the operator
+    // is 'IN', FALSE is returned
+    $test_array_data = array('Llama');
+    $test_array_value = array('Kitten');
+    $condition = $this->conditionManager->createInstance('rules_data_comparison')
+      ->setContextValue('data', $test_array_data)
+      ->setContextValue('op', 'IN')
+      ->setContextValue('value', $test_array_value);
+    $this->assertFALSE($condition->execute());
+
+    // Test that when data is greater than value and operator is '>', 
+    // TRUE is returned
+    $test_integer_data = 2;
+    $test_integer_value = 1;
+    $condition = $this->conditionManager->createInstance('rules_data_comparison')
+      ->setContextValue('data', $test_integer_data)
+      ->setContextValue('op', '>')
+      ->setContextValue('value', $test_integer_value);
+    $this->assertTRUE($condition->execute());
+
+    // Test that when data is less than value and operator is '>', 
+    // FALSE is returned
+    $test_integer_data = 1;
+    $test_integer_value = 2;
+    $condition = $this->conditionManager->createInstance('rules_data_comparison')
+      ->setContextValue('data', $test_integer_data)
+      ->setContextValue('op', '>')
+      ->setContextValue('value', $test_integer_value);
+    $this->assertFALSE($condition->execute());
+
+    // Test that when data is less than value and operator is '<', 
+    // TRUE is returned
+    $test_integer_data = 1;
+    $test_integer_value = 2;
+    $condition = $this->conditionManager->createInstance('rules_data_comparison')
+      ->setContextValue('data', $test_integer_data)
+      ->setContextValue('op', '<')
+      ->setContextValue('value', $test_integer_value);
+    $this->assertTRUE($condition->execute());
+
+    // Test that when data is greater than value and operator is '<', 
+    // FALSE is returned
+    $test_integer_data = 2;
+    $test_integer_value = 1;
+    $condition = $this->conditionManager->createInstance('rules_data_comparison')
+      ->setContextValue('data', $test_integer_data)
+      ->setContextValue('op', '<')
+      ->setContextValue('value', $test_integer_value);
+    $this->assertFALSE($condition->execute());
+
+  }
+
+}
